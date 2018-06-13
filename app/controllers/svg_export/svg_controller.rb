50b4886748f2fd69d7cc14bced6f0f1cd106eec5
Stefan Wienert <info@stefanwienert.de>
2018-06-13 09:05:24 +0200

    0.1.0.pre - Rails 5.1

--- app/controllers/svg_export/svg_controller.rb
+++ app/controllers/svg_export/svg_controller.rb
@@ -1,20 +1,19 @@
 module SvgExport
   class SvgController < ApplicationController
-    skip_before_filter :verify_authenticity_token
+    skip_before_action :verify_authenticity_token
 
     def create
       if Engine.batik_path.blank?
-        raise ArgumentError.new("Set SvgExport::Engine.batik_path = '...' to the correct path of the batik-rasterizer")
+        raise ArgumentError, "Set SvgExport::Engine.batik_path = '...' to the correct path of the batik-rasterizer"
       end
       wrapper = CommandWrapper.new(params, base_url: request.referer)
       begin
-        file = wrapper.()
+        file = wrapper.call
       rescue SvgExport::Error => e
-        render :text => "Unable to export image;\n #{e}", status: 422
+        render plain: "Unable to export image;\n #{e}", status: :unprocessable_entity
         return
       end
       send_file file, type: wrapper.type, filename: wrapper.filename, disposition: 'attachment', stream: false
-
     end
   end
 end
